e5d90ab2ea4ceb9806744c12680a3fb3d09e5f63
Lee <leeirungu11313@gmail.com>
2024-03-28 12:50:56 +0300

    Add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  let input =document.getElementById("new-task-description","list","tasks")
+  form.addEventListener("submit",(e) => {
+    e.preventDefault()
+    if (input.value.trim() === ""){
+      alert("Please enter a task description.")
+      return;
+    }
+    let par = document.createElement("p")
+    par.textContent = input.value 
+    let list = document.getElementById("list")
+    list.appendChild(par)
+    input.value = ""
+  })
 });
